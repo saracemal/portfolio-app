ca577acb139880b2685176e51233a0ff4be86683
Sara Cemal <saracemal@gmail.com>
2021-05-19 22:46:17 -0400

    changed navbar back to bootstrap

--- src/HomeNav.js
+++ src/HomeNav.js
@@ -1,23 +1,40 @@
 import React from "react";
-import './NavBar.css'
-import styled from 'styled-components'
+import './NavBar.css';
+import styled from 'styled-components';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
 
 
 // find photos for the links 
 function HomeNav({sticky}) {
     return (
-        <nav className={sticky ? "navbar navbar-sticky" : "navbar"}>
-        <div className="navbar--logo-holder">
-        {sticky ? <H1>sara cemal 🐞</H1> : null}
-        <H1>sara cemal 🐞</H1>
-        </div>
-        <ul className="navbar--link">
-        <li className="navbar--link-item">LinkedIn</li>
-        <li className="navbar--link-item">Github</li>
-        <li className="navbar--link-item">Medium</li>
-        <li className="navbar--link-item">Resume</li>
-        </ul>
-    </nav>
+    //     <nav className={sticky ? "navbar navbar-sticky" : "navbar"}>
+    //     <div className="navbar--logo-holder">
+    //     {sticky ? <H1>sara cemal 🐞</H1> : null}
+    //     <H1>sara cemal 🐞</H1>
+    //     </div>
+    //     <ul className="navbar--link">
+    //     <li className="navbar--link-item">LinkedIn</li>
+    //     <li className="navbar--link-item">Github</li>
+    //     <li className="navbar--link-item">Medium</li>
+    //     <li className="navbar--link-item">Resume</li>
+    //     </ul>
+    // </nav>
+
+        <Navbar bg="light" expand="lg">
+        <Navbar.Brand><H1>sara cemal 🐞</H1></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+            <Nav.Link href="#home">resume</Nav.Link>
+            <Nav.Link href="linkedin.com/in/saracemal">linkedin</Nav.Link>
+            <Nav.Link href="github.com/saracemal">github</Nav.Link>
+            <Nav.Link href="saracemal.medium.com">medium</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+        </Navbar>
     )
 }
 
--- src/InfoSection.js
+++ src/InfoSection.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from 'styled-components';
 
-
+// add padding
 function InfoSection() {
     return (
         <Container>
-            <H1>hello! I'm Sara Cemal, a full-stack software engineer based in NYC</H1>
+            <H1>hello! I'm Sara Cemal, a full-stack software engineer based in NYC 🌃</H1>
             <Info>
                 <p>What do sociology, neuroscience, and coding have in common? The ability to critically think, analyze, and problem solve until you reach the root of the problem. I enjoy combining my background into passion projects, the best part being that I can create these applications all on my own! </p>
                 <p>Fun facts: I will beat you in a game of Geoguessr, I've created the perfect capsule wardrobe, my YouTube recommended page consists mainly of travel vloggers and tiny desk concerts, and I will always ask for extra spice on my food.</p>
