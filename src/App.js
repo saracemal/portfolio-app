c2f4bbc1c1f36c736c7fb4901c702caac15bcf6b
Sara Cemal <saracemal@gmail.com>
2021-06-17 17:01:29 -0400

    removed sticky nav bar

--- src/App.js
+++ src/App.js
@@ -2,30 +2,27 @@ import React from "react";
 import HomePage from "./HomePage"
 import HomeNav from "./HomeNav"
 import './data';
-import UseSticky from "./UseSticky.js"
+
 
 function App() {
-  const { isSticky, element } = UseSticky()
+
   //fetch for projects right here 
     return (
-        <div>
-          <HomeNav sticky={isSticky} />
-          <HomePage element={element} />
-        </div>
-            // <Switch>
-            //     <Route exact path="/">
-            //       <HomePage />
-            //     </Route>
-            //     <Route exact path="/">
-            //       <HomePage />
-            //     </Route>
-            //     <Route exact path="/projects">
-            //       <ProjectPage />
-            //     </Route>
-            //     <Route exact path="/resume">
-            //       <ResumePage />
-            //     </Route>
-            // </Switch>
+       
+            <Switch>
+                <Route exact path="/">
+                  <HomePage />
+                </Route>
+                <Route exact path="/">
+                  <HomePage />
+                </Route>
+                <Route exact path="/projects">
+                  <ProjectPage />
+                </Route>
+                <Route exact path="/resume">
+                  <ResumePage />
+                </Route>
+            </Switch>
         
     )
 }
