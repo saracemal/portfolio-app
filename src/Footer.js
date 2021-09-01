37e41a40e88aa0bf946424e764d6c23ef9f5ec5b
Sara Cemal <saracemal@gmail.com>
2021-09-01 14:37:33 -0400

    added more links to bottom of footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import gitImage from '../footerimages/github.png'
+import linkedinImage from '../footerimages/linkedin.png'
 
 function Footer() {
     return (
         <Wrapper>
-            <Text>created with 💟 by <a href="linkedin.com/saracemal">Sara Cemal</a></Text>
+            <Text>created with 💟  by <a href="linkedin.com/saracemal">Sara Cemal</a></Text>
+            <a href="https://www.linkedin.com/in/saracemal">
+            <img src={linkedinImage} alt="linkedin.com" width="100" height="132"/>
+            </a> & 
+            <a href="https://www.github.com/saracemal">
+            <img src={gitImage} alt="github.com" width="100" height="132"/>
+            </a>
         </Wrapper>
     )
 }
